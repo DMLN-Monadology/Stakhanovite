3d3db99e7e2ff3849a25091b10f8a095285b8f89
Stian Nguyen <Lightning@Stians-MacBook-Pro.local>
2016-11-01 13:46:18 -0700

    finishing backend authentication. One is now able to sign up, log in, log out using $.ajax commands on the console

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+              params[:user][:username],
+              params[:user][:password]
+    )
+
+    if @user
+      sign_in(@user)
+      render "api/users/show"
+    else
+      render(json: ["Invalid username/password"], status: 401)
+    end
+  end
+
+  def destroy
+    if current_user
+      sign_out
+      # render "api/sessions/new"
+    else
+      render(json: ["Not Signed in"], status: 404)
+    end
+  end
+end
