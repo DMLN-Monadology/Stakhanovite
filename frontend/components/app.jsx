0fbe4314ece120de509784bd0b8b2ca1004fbbc7
Stian Nguyen <Lightning@Stians-MacBook-Pro.local>
2016-11-01 18:41:04 -0700

    finishing implementation of root and app into render as home path

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = ({children}) => (
+  <div>
+    <h1>The App for the Worldwide Stakhanovites!</h1>
+    {children}
+  </div>
+);
+
+export default App;
--- frontend/stakhanovite.jsx
+++ frontend/stakhanovite.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-
-import {signup, signin, signout} from './actions/session_actions';
+import Root from './components/root';
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,19 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore()
   const root = document.getElementById('root');
 
-  ReactDOM.render(<h1>Welcome to Stakhanovite, under Construction</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 
-  window.store = configureStore()
-
-  window.success = () => {return console.log("success!");};
-  window.error = () => {return console.log("failure!");};
-  window.params = {
-      username: "Vatutin",
-      password: "By"
-  }
-
-
-  window.signup = signup;
-  window.signin = signin;
-  window.signout = signout;
 });
