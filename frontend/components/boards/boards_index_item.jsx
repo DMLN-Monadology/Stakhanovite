8f8504d5732732df17f9e3ae1b9d9b7589925708
Duc (Stian) M. L. Nguyen <duc.m.nguyen@yale.edu>
2016-12-15 13:02:44 -0800

    CSS styling for Board dropdown button

--- frontend/components/boards/boards_index_item.jsx
+++ frontend/components/boards/boards_index_item.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router';
 class BoardIndexItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      BoardDropdown: "closed"
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -12,13 +15,30 @@ class BoardIndexItem extends React.Component {
     this.props.deleteBoard(this.props.board.id);
   }
 
+  toggleBoardDropdown() {
+    return (event) => {
+      event.preventDefault();
+      this.state.BoardDropdown === "closed" ?
+      this.setState({ BoardDropdown: "open"}) :
+      this.setState({ BoardDropdown: "closed"})
+    }
+  }
+
   render() {
+      let DDButton = (
+        <p onClick={this.toggleBoardDropdown()}
+          className="BoardDDButton">...</p>
+      )
     let deleteBoardDD
-    if (this.props.owned == "true") {
+    if ((this.props.owned == "true") & this.state.BoardDropdown === "open") {
       deleteBoardDD = (
-        <button onClick={this.handleSubmit}>
-          -
-        </button>
+        <div className="BoardDD">
+          <h3>Board Options</h3>
+          <button onClick={this.toggleBoardDropdown()} className="BoardDDReturn">X</button>
+          <p onClick={this.handleSubmit} className="deleteBoardButton">
+            DELETE THIS BOARD
+          </p>
+        </div>
       )
     }
     return (
@@ -26,6 +46,7 @@ class BoardIndexItem extends React.Component {
         <Link to={`/boards/owner/${this.props.board.id}`}>
           {this.props.board.title}
         </Link>
+        {DDButton}
         {deleteBoardDD}
       </li>
     )
